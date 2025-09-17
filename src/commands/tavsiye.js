de5dab4f0b76ad5c64ebea4e7642d8da5037e540
owomed <owomedsunucu@gmail.com>
2025-09-17 12:13:44 +0300

    Update tavsiye.js

--- src/commands/tavsiye.js
+++ src/commands/tavsiye.js
@@ -1,6 +1,4 @@
-// src/commands/tavsiye.js
-const { MessageEmbed } = require('discord.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 // Sabit kanal ID'si
 const TAVSIYE_KANAL_ID = '1235593289315651685';
@@ -18,64 +16,53 @@ module.exports = {
     // Prefix komutu için isim ve açıklama
     name: 'tavsiye',
     description: 'Sunucu tavsiyesi gönderir.',
-    aliases: ['öneri'], // İsteğe bağlı, ek takma ad ekleyebilirsiniz.
+    aliases: ['öneri'],
 
-    // Komutun ana mantığını yürüten bir fonksiyon oluşturalım.
+    // Komutun ana mantığını yürüten bir fonksiyon
     async handleTavsiyeCommand(interactionOrMessage, tavsiyeMesaji) {
         const client = interactionOrMessage.client;
         const tavsiyeKanal = client.channels.cache.get(TAVSIYE_KANAL_ID);
         const user = interactionOrMessage.author || interactionOrMessage.user;
 
         if (!tavsiyeKanal) {
-            return interactionOrMessage.reply ? await interactionOrMessage.reply('Tavsiye kanalı bulunamadı. Lütfen kanal ID\'sinin doğru olduğundan emin olun.') :
-                interactionOrMessage.channel.send('Tavsiye kanalı bulunamadı. Lütfen kanal ID\'sinin doğru olduğundan emin olun.');
+            const errorMessage = 'Tavsiye kanalı bulunamadı. Lütfen kanal ID\'sinin doğru olduğundan emin olun.';
+            if (interactionOrMessage.isChatInputCommand()) {
+                await interactionOrMessage.reply({ content: errorMessage, ephemeral: true });
+            } else {
+                await interactionOrMessage.channel.send(errorMessage);
+            }
+            return;
         }
 
         const zamanDamgasi = new Date().toLocaleString('tr-TR', { timeZone: 'Europe/Istanbul', hour12: false, hour: '2-digit', minute: '2-digit' });
 
-        const embed = new MessageEmbed()
+        const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('MED Mod Yardım')
-            .setDescription(`**${user} Tavsiye etti 📬**
-
-Kullanıcı ID: \`${user.id}\`
-Kullanıcı: \`${user.username}\`
-            
-**Tavsiye Mesajı:**
-${tavsiyeMesaji}
-            
-*Kullanıldığı zaman:*
-${zamanDamgasi}`)
-            .setThumbnail(user.displayAvatarURL({ format: 'png', dynamic: true }))
-            .setFooter('Tavsiyeleriniz bizim için değerli ❤️');
+            .setDescription(`**${user} Tavsiye etti 📬**\n\nKullanıcı ID: \`${user.id}\`\nKullanıcı: \`${user.username}\`\n\n**Tavsiye Mesajı:**\n${tavsiyeMesaji}\n\n*Kullanıldığı zaman:*\n${zamanDamgasi}`)
+            .setThumbnail(user.displayAvatarURL({ forceStatic: false }))
+            .setFooter({ text: 'Tavsiyeleriniz bizim için değerli ❤️' });
 
         try {
-            // Tavsiye mesajını tavsiye kanalına gönder
             const tavsiyeMesaj = await tavsiyeKanal.send({ embeds: [embed] });
 
-            // Emojileri tavsiye mesajına ekle
             await tavsiyeMesaj.react('✅');
             await tavsiyeMesaj.react('❎');
 
-            // Kullanıcıya geri bildirim mesajı gönder
             const feedbackMessage = `Tavsiye mesajı <#${TAVSIYE_KANAL_ID}> kanalına gönderildi.Teşekkür ediyoruz ${user} <:cute_owo:1246376595921436724>`;
             
-            if (interactionOrMessage.reply) {
-                // Slash komutları için görünür yanıt
+            if (interactionOrMessage.isChatInputCommand()) {
                 await interactionOrMessage.reply({ content: feedbackMessage, ephemeral: false });
             } else {
-                // Prefix komutları için orijinal mesajı sil ve yeni mesaj gönder
                 if (interactionOrMessage.deletable) {
                     await interactionOrMessage.delete();
                 }
                 await interactionOrMessage.channel.send(feedbackMessage);
             }
-
         } catch (error) {
             console.error('Tavsiye gönderilirken bir hata oluştu:', error);
-            // Hata durumunda kullanıcıya geri bildirim gönder
             const errorMessage = 'Tavsiye gönderilirken bir hata oluştu.';
-            if (interactionOrMessage.reply) {
+            if (interactionOrMessage.isChatInputCommand()) {
                 await interactionOrMessage.reply({ content: errorMessage, ephemeral: true });
             } else {
                 await interactionOrMessage.channel.send(errorMessage);
