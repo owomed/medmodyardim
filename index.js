1acdee2684270e25bfd071423fabfdf82f236cc5
owomed <owomedsunucu@gmail.com>
2025-09-19 21:36:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
     if (event.once) {
-        client.once(event.name, (...args) => event.execute(client, ...args));
+        client.once(event.name, (...args) => event.execute(...args));
     } else {
-        client.on(event.name, (...args) => event.execute(client, ...args));
+        client.on(event.name, (...args) => event.execute(...args));
     }
 }
 
